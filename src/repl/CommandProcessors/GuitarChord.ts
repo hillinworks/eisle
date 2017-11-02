8c103b321c94d2ffa1ebc4db2e006f93bac6e715
hillin <hillinsilence@gmail.com>
2017-11-02 11:45:28 +0800

    encode chord name in url

--- src/repl/CommandProcessors/GuitarChord.ts
+++ src/repl/CommandProcessors/GuitarChord.ts
@@ -104,7 +104,7 @@ export class GuitarChord implements ICommandProcessor {
             title: ChordName.getOrdinalNamePlain(chord),
             description: "点击查看详情",
             picUrl: `${titleImagePath}?${Date.now()})}`,
-            url: `${Server.host}/chord/${chordName}?${Date.now()}`,
+            url: `${Server.host}/chord/${encodeURIComponent(chordName)}?${Date.now()}`,
         });
     }
 
